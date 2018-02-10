25cd078ba4eba0b0388031cb1006724407b6fa0e
mohamed <moh4med.ahmed@gmail.com>
2018-02-10 13:46:58 +0200

    pass message to child activity& return result.

---
+++ app/src/main/java/com/example/mohamed/playground/DetailActivity.java
@@ -0,0 +1,51 @@
+package com.example.mohamed.playground;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    TextView tv_name;
+    private Intent returnintent;
+    EditText et_message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        tv_name=(TextView)findViewById(R.id.tv_name);
+        et_message=(EditText) findViewById(R.id.et_message);
+        String name=getIntent().getStringExtra("name");
+        tv_name.setText(name);
+        returnintent=new Intent();
+        setResult(Activity.RESULT_OK,returnintent);
+    }
+
+    /*@Override
+    public boolean onSupportNavigateUp() {
+        String message=et_message.getText().toString();
+        returnintent.putExtra("message",message);
+        finish();
+        return true;
+    }*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                String message=et_message.getText().toString();
+                returnintent.putExtra("message",message);
+               // NavUtils.navigateUpFromSameTask(this);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+   
+}
--- app/src/main/java/com/example/mohamed/playground/MainActivity.java
+++ app/src/main/java/com/example/mohamed/playground/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.mohamed.playground;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int DETAIL_ACTIVITY_REQUEST_CODE = 1;
+    TextView tv_message;
+    private EditText et_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_message=findViewById(R.id.tv_name);
+        et_name=(EditText) findViewById(R.id.et_name);
+    }
+
+    public void onclick(View view) {
+        switch (view.getTag().toString()) {
+            case "detail": {
+                Intent intent = new Intent(this.getApplicationContext(), DetailActivity.class);
+                String name=et_name.getText().toString();
+                intent.putExtra("name", name);
+                startActivityForResult(intent, DETAIL_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case DETAIL_ACTIVITY_REQUEST_CODE:{
+                if(resultCode== Activity.RESULT_OK){
+                    String message = data.getStringExtra("message");
+                    tv_message.setText(message);
+                }else{
+
+                }
+
+                break;
+            }
+        }
     }
 }

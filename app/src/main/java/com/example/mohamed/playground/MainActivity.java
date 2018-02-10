cdb3f0f4eaa516173ae53ca6d80a907c862f50cd
mohamed <moh4med.ahmed@gmail.com>
2018-02-10 16:48:55 +0200

    get all app that handle the intent

--- app/src/main/java/com/example/mohamed/playground/MainActivity.java
+++ app/src/main/java/com/example/mohamed/playground/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.mohamed.playground;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int DETAIL_ACTIVITY_REQUEST_CODE = 1;
     TextView tv_message;
     private EditText et_name;
+    private String TAG=MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this.getApplicationContext(), DetailActivity.class);
                 String name = et_name.getText().toString();
                 intent.putExtra("name", name);
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
                 startActivityForResult(intent, DETAIL_ACTIVITY_REQUEST_CODE);
                 break;
             }
             case "view": {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 Intent chooser = Intent.createChooser(intent, "choose app");
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     //            startActivity(chooser);         //to create specific choose with no default button
                     startActivity(intent);
@@ -47,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent("com.example.mohamed.playground.DUMMY");
                 intent.putExtra("first", "mohamed");
                 intent.putExtra("second", "ahmed");
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 } else {
@@ -58,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent("com.example.mohamed.playground.DUMMYREVERSE");
                 intent.putExtra("first", "ali");
                 intent.putExtra("second", "hassan");
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
                 } else {
@@ -70,8 +92,26 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("first", "ezz");
                 intent.putExtra("second", "deghedy");
                 Intent chooser = Intent.createChooser(intent, "choose app");
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
+                }
+                break;
+
+            }case "any": {
+                Intent intent = new Intent("");
+                intent.addCategory("android.intent.category.DEFAULT");
+                List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_ALL);
+                for(ResolveInfo x:resolveInfos){
+                    Log.e(TAG,x.activityInfo.packageName);
+                }
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "no app found", Toast.LENGTH_SHORT).show();
                 }
                 break;
             }
